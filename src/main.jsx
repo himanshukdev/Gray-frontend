a38bc68caf1ae38c672f28a09320acdfa950c7fb
Himanshu K <himanshuk.dev@gmail.com>
2024-04-23 03:19:42 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
+import Task from "./pages/Task";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 import store from "./redux/store";
 import "./index.css";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Registration />,
+  },
+  {
+    path: "/to-do",
+    element: <Task />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
     ,
   </React.StrictMode>
